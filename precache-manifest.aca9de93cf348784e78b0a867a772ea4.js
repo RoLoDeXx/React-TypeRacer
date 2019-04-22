c2228555435afa7bf36f9cf2753e03eca4281793
RoLoDeXx <dexxrolo@gmail.com>
2019-04-23 01:43:08 +0530

    Updates

---
+++ precache-manifest.aca9de93cf348784e78b0a867a772ea4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da59b58734641573fafa",
+    "url": "/React-TypeRacer/static/css/main.b81e7461.chunk.css"
+  },
+  {
+    "revision": "da59b58734641573fafa",
+    "url": "/React-TypeRacer/static/js/main.263d52e0.chunk.js"
+  },
+  {
+    "revision": "c2c1646123ead47bcc30",
+    "url": "/React-TypeRacer/static/js/runtime~main.f44de202.js"
+  },
+  {
+    "revision": "e90574d74274c7fb4d49",
+    "url": "/React-TypeRacer/static/js/2.c39fed63.chunk.js"
+  },
+  {
+    "revision": "e809a75d76028ae89762b54819988edd",
+    "url": "/React-TypeRacer/index.html"
+  }
+];

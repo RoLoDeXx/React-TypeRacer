c9e2a8cfeca8eaf8f75e69246eae6edf560369fd
RoLoDeXx <RoLoDeXx@gmail.com>
2021-01-17 15:06:45 +0530

    Added timer

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,9 +8,9 @@ const Main = () => {
   const [query, setQuery] = useState("");
   const [target, setTarget] = useState("");
   const [stash, setStash] = useState([]);
+  const [et, setEt] = useState(0);
+  const [st, setSt] = useState(0);
 
-  let time;
-  let timeoutId;
   let colorBlocks = document.querySelectorAll(".neutral");
 
   useEffect(() => {
@@ -34,58 +34,56 @@ const Main = () => {
 
     if (query === target.slice(0, query.length) && query.length !== 0) {
       colorBlocks.forEach((block) => block.classList.add("error", "success"));
-      if (query.length === target.length) {
-        clearInterval(timeoutId);
+      if (query === target) {
         setShowInstructions(false);
+        let time = new Date();
+        setEt(time.getTime());
       }
     } else colorBlocks.forEach((block) => block.classList.remove("success"));
   };
 
   const startGame = () => {
-    setShowInstructions(true);
-    setQuery("");
-    time = 0;
+    setSt(0);
+    setEt(0);
     colorBlocks.forEach((block) => block.classList.remove("success", "error"));
-
     let indx = Math.floor(Math.random() * 102);
     setTarget(stash[indx].quote);
-    timeoutId = setInterval(() => {
-      ++time;
-    }, 1000);
+    setShowInstructions(true);
+    setQuery("");
+    let time = new Date();
+    setSt(time.getTime());
   };
 
   return (
     <div>
-      <div>
-        <div className="div1">
-          {target === "" ? (
-            <p className="targetText">Click on start to begin!</p>
-          ) : (
-            <p className="targetText">{target}</p>
-          )}
-        </div>
-
-        <div className="typeForm">
-          <input
-            type="text"
-            placeholder="Start typing here..."
-            autoComplete="off"
-            value={query}
-            onChange={(e) => handleChange(e)}
-            disabled={target === ""}
-          />
-          <br />
-          <button onClick={startGame} className="neutral">
-            TypeRace
-          </button>
-        </div>
-
-        <ScoreCard
-          showInstructions={showInstructions}
-          time={time}
-          length={target.split(" ").length + 1}
-        />
+      <div className="div1">
+        {target === "" ? (
+          <p className="targetText">Click on start to begin!</p>
+        ) : (
+          <p className="targetText">{target}</p>
+        )}
       </div>
+
+      <div className="typeForm">
+        <input
+          type="text"
+          placeholder="Start typing here..."
+          autoComplete="off"
+          value={query}
+          onChange={(e) => handleChange(e)}
+          disabled={target === ""}
+        />
+        <br />
+        <button onClick={startGame} className="neutral">
+          TypeRace
+        </button>
+      </div>
+
+      <ScoreCard
+        showInstructions={showInstructions}
+        time={(et - st) / 1000}
+        length={target.split(" ").length + 1}
+      />
     </div>
   );
 };

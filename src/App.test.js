743b34841a178258d47e4d733e5ae0193b52b25a
RoLoDeXx <RoLoDeXx@gmail.com>
2021-01-19 22:25:13 +0530

    dummy test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import * as React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./components/App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkElement = screen.getByText(/Click on start to begin!/i);
+  expect(10).toBeGreaterThan(5);
 });
